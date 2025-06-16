5c42d4fa4b3017b2efd4eb070e8faa7517bebcc4
op many <afekzigdon@gmail.com>
2025-06-16 19:45:19 +0300

    fixed client build problems

--- client/src/pages/DBHandler.js
+++ client/src/pages/DBHandler.js
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import { WordDB, getCurrWordBank } from "../WordDB";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { getConnection } from "../apiRequest";
-import React, { useState } from "react";
-
-const [connectionStatus, setConnectionStatus] = useState(null);
-
-async function testConnection() {
-  try {
-    const data = await getConnection();
-    
-    setConnectionStatus(data.message);
-  } catch (err) {
-    setConnectionStatus("Backend unreachable 😢");
-  }
-}
+import { getConnection } from "../ApiRequest";
 
 const DBHandler = (props) => {
+  const [connectionStatus, setConnectionStatus] = useState(null);
+
+  async function testConnection() {
+    try {
+      const data = await getConnection();
+
+      setConnectionStatus(data.message);
+    } catch (err) {
+      setConnectionStatus("Backend unreachable 😢");
+    }
+  }
+
   return (
     <Container className="text-center mt-5">
       <h1>This is the DB Handler</h1>
