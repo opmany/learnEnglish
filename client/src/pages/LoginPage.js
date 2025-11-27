9584a4ba1e05052b843ca8ffccfa5fe05d6f058f
op many <afekzigdon@gmail.com>
2025-11-27 19:35:37 +0200

    added auto go into home page if login was already made and slipped through

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,12 +1,22 @@
 // src/pages/Login.jsx
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router";
 import AuthForm from "../components/AuthForm";
 import { useUser } from "../contexts/UserContext";
 
 export default function Login() {
-  const { user, logout, login } = useUser();
+  const { user, login } = useUser();
   const navigate = useNavigate();
+
+  const checkUserConnection = () => {
+    if (user) {
+    navigate("/");
+    }
+  };
+
+  useEffect(() => {
+      checkUserConnection();  
+  }, []);
 
   const handleLogin = async ({ username, password }) => {
     await login(username, password);
